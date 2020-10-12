e1c10fbdb4330fc7fa9cac6985d02bf3c4c212de
Logophilist <Ralf@van-aert.nl>
2020-10-12 15:09:29 +0200

    SaveData main push

--- PWManager Server/SaveData.cs
+++ PWManager Server/SaveData.cs
@@ -1,42 +1,128 @@
-﻿using System;
+﻿using General;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 using System.Text;
+using System.Threading;
 
 namespace Server
 {
     class SaveData
     {
         private string path;
-        private string filename;
-        public SaveData(string path, string filename)
+        private dynamic json;
+        private const string jsonFilename = "/json.txt";
+        public SaveData(string path)
         {
             this.path = path;
-            this.filename = filename;
+            this.ReadJSON();
+        }
+
+        public void ReadJSON()
+        {
             if (!Directory.Exists(path))
             {
                 Directory.CreateDirectory(path);
+                json = new { };
+            }
+            else
+            {
+                string file = "";
+                using (StreamReader sr = new StreamReader(this.path + jsonFilename))
+                {
+                    string line;
+                    while ((line = sr.ReadLine()) != null)
+                    {
+                        file += line;
+                    }
+                    json = JsonConvert.DeserializeObject(file);
+                }
             }
         }
 
-        /// <summary>
-        ///  Every line is a new data entry
-        /// </summary>
-       
         public void WriteDataJSON(string data)
         {
-            using (StreamWriter sw = File.AppendText(this.path + "/json"+filename+".txt"))
+            using (StreamWriter sw = File.AppendText(this.path + jsonFilename))
             {
                 sw.WriteLine(data);
             }
         }
 
-        public void WriteDataRAW(string data)
+        public List<LoginCredentials> GetLoginsUser(string mUsername, string mPassword)
         {
-            using (StreamWriter sw = File.AppendText(this.path + "/raw" + filename + ".txt"))
+            JArray userArray = JArray.Parse(JsonConvert.SerializeObject(json.data));
+            bool foundUser = false;
+            JArray userData = new JArray();
+
+            foreach (var user in userArray.Children())
             {
-                sw.WriteLine(data);
+                var userProp = user.Children<JProperty>();
+                if (userProp.FirstOrDefault(x => x.Name == "username").Value.ToString() == mUsername && userProp.FirstOrDefault(x => x.Name == "password").Value.ToString() == mPassword && !foundUser)
+                {
+                    userData = (JArray)user.Children<JProperty>().FirstOrDefault(x => x.Name == "data").Value;
+                    foundUser = true;
+                }
             }
+            List<LoginCredentials> returnList = new List<LoginCredentials>();
+            if (foundUser)
+            {
+                foreach (dynamic user in userData.Children())
+                {
+                    returnList.Add(new LoginCredentials((string)user.place, (string)user.username, (string)user.password));
+                }
+            }
+
+            return returnList;
+        }
+
+        public void Add(string mUsername, string mPassword, LoginCredentials login)
+        {
+            dynamic dynLogin = new
+            {
+                place = login.Place,
+                username = login.Username,
+                password = login.Password
+            };
+
+            JArray userArray = JArray.Parse(JsonConvert.SerializeObject(json.data));
+            bool foundUser = false;
+
+            foreach (var user in userArray.Children())
+            {
+
+                var userProp = user.Children<JProperty>();
+                if (userProp.FirstOrDefault(x => x.Name == "username").Value.ToString() == mUsername && userProp.FirstOrDefault(x => x.Name == "password").Value.ToString() == mPassword && !foundUser)
+                {
+                    JArray userData = (JArray)user.Children<JProperty>().FirstOrDefault(x => x.Name == "data").Value;
+
+                    userData.Add(JObject.Parse(JsonConvert.SerializeObject(dynLogin)));
+                    foundUser = true;
+                }
+
+            }
+            if (!foundUser)
+            {
+                dynamic newUser = new
+                {
+                    username = mUsername,
+                    password = mPassword,
+                    data = new dynamic[]
+                    {
+                        dynLogin
+                    }
+                };
+                userArray.Add(JObject.Parse(JsonConvert.SerializeObject(newUser)));
+            }
+
+            dynamic newJson = new
+            {
+                data = userArray
+            };
+            json = newJson;
         }
     }
 }
+

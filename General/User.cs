13e702f1e31d91be6a8d0c9acc2ab68cfb3cc06a
Logophilist <Ralf@van-aert.nl>
2020-10-12 15:10:49 +0200

    Dummy file/push

---
+++ General/User.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace General
+{
+    class User
+    {
+        private string username;
+        private string password;
+        private List<LoginCredentials> logins;
+
+        public User(string username, string password)
+        {
+            this.username = username;
+            this.password = password;
+        }
+
+    }
+}

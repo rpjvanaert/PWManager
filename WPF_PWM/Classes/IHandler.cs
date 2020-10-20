4bb21e2f963e6a84816bffd8824a3ed635e204d7
Logophilist <Ralf@van-aert.nl>
2020-10-20 14:47:58 +0200

    IHandler for callback

---
+++ WPF_PWM/Classes/IHandler.cs
@@ -0,0 +1,18 @@
+﻿using General;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WPF_PWM.Classes
+{
+    public interface IHandler
+    {
+        void giveData(List<LoginCredentials> logins);
+
+        void Login(bool status);
+
+        void stop();
+    }
+}
